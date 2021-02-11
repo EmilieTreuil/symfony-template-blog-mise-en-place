b0b1bafe9c43fc68cc9197a9ef29722a7332bf52
Emilie Treuil <emilie.treuil@gmail.com>
2021-02-11 16:56:40 +0100

    Ajout de catégories, de slug dans les url et d'une partie admin

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,33 +2,36 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PostController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
+        $posts = $postRepository->findAll();
+        // dd($posts);
+
         return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController HOME',
+            'posts' => $posts,
         ]);
     }
 
      /**
-     * @Route("/post/{id}", name="post_view", methods={"GET"}, requirements={"id"="\d+"})
+     * Route("/post/{id}", name="post_view", methods={"GET"}, requirements={"id"="\d+"})
+     * @Route("/post/{slug}", name="post_view", methods={"GET"})
      */
-    public function post($id): Response
+    public function post(Post $post): Response
     {
         return $this->render('post/post.html.twig', [
-            'post' => [
-                'title' => 'Le titre de l\'article',
-                'content' => 'Le contenu de l\'article',
-                'createdAt' => '09/02/2021',
-            ],
+                'post' => $post
         ]);
     }
+
 }
